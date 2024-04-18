7d2d77cbe7ee7b310da9b2318c017d3e8e3138c0
Sagar Kumar <krsk987@gmail.com>
2024-04-18 16:35:45 +0530

    ...

--- src/main/java/com/sagar/todoapp/Repositories/TodoItemRepository.java
+++ src/main/java/com/sagar/todoapp/Repositories/TodoItemRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TodoItemRepository extends JpaRepository<TodoItem, Integer> {
+public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
+
 }
--- src/main/java/com/sagar/todoapp/Services/TodoItemServices.java
+++ src/main/java/com/sagar/todoapp/Services/TodoItemServices.java
@@ -1,4 +1,11 @@
 package com.sagar.todoapp.Services;
 
+import com.sagar.todoapp.Repositories.TodoItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class TodoItemServices {
+    @Autowired
+    private TodoItemRepository todoItemRepository;
+
+    public
 }
