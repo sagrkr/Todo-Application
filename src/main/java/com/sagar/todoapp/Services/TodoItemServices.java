913b59b7ed5a78fd7d33b30112eca37cced25566
Sagar Kumar <krsk987@gmail.com>
2024-04-18 17:03:29 +0530

    TodoItemServices completed

--- src/main/java/com/sagar/todoapp/Services/TodoItemServices.java
+++ src/main/java/com/sagar/todoapp/Services/TodoItemServices.java
@@ -1,11 +1,35 @@
 package com.sagar.todoapp.Services;
 
 import com.sagar.todoapp.Repositories.TodoItemRepository;
+import com.sagar.todoapp.models.TodoItem;
+import org.aspectj.apache.bcel.classfile.Module;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.Instant;
+import java.util.Optional;
 
 public class TodoItemServices {
     @Autowired
     private TodoItemRepository todoItemRepository;
 
-    public
+    public Iterable<TodoItem> getAll(){
+        return todoItemRepository.findAll();
+    }
+
+    public Optional<TodoItem> getById(Long id){
+        return todoItemRepository.findById(id);
+    }
+
+    public TodoItem save(TodoItem todoItem){
+        if(todoItem.getId() == null){
+            todoItem.setCreatedAt(Instant.now());
+        }
+        todoItem.setUpdatedAt(Instant.now());
+        return todoItemRepository.save(todoItem);
+    }
+
+    public void delete(TodoItem todoItem){
+        todoItemRepository.delete(todoItem);
+    }
 }
+
