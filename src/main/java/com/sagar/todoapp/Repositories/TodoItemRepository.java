844490cec69f004823c1d83b4ce145b9d10ff1a4
Sagar Kumar <krsk987@gmail.com>
2024-04-18 16:24:12 +0530

    repositories created

---
+++ src/main/java/com/sagar/todoapp/Repositories/TodoItemRepository.java
@@ -0,0 +1,10 @@
+package com.sagar.todoapp.Repositories;
+
+
+import com.sagar.todoapp.models.TodoItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoItemRepository extends JpaRepository<TodoItem, Integer> {
+}
